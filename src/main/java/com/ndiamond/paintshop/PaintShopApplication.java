84d09c8e461f88cc64a9b2fa9af6798b2314f42e
izyyy <31731867+izyyy@users.noreply.github.com>
2026-03-27 13:15:43 +0100

    minor .env update

--- src/main/java/com/ndiamond/paintshop/PaintShopApplication.java
+++ src/main/java/com/ndiamond/paintshop/PaintShopApplication.java
@@ -14,13 +14,13 @@ public class PaintShopApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-
-		System.setProperty("jwtSecret", dotenv.get("jwtSecret"));
+//		Dotenv dotenv = Dotenv.load();
+//
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//
+//		System.setProperty("jwtSecret", dotenv.get("jwtSecret"));
 
 		SpringApplication.run(PaintShopApplication.class, args);
 
