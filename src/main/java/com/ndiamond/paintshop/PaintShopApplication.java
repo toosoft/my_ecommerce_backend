8966959b3565de3cb247da07817f4decf19f4456
izyyy <31731867+izyyy@users.noreply.github.com>
2026-03-27 16:26:59 +0100

    connection

--- src/main/java/com/ndiamond/paintshop/PaintShopApplication.java
+++ src/main/java/com/ndiamond/paintshop/PaintShopApplication.java
@@ -3,7 +3,7 @@ package com.ndiamond.paintshop;
 //import io.github.cdimascio.dotenv.Dotenv;
 //import io.jsonwebtoken.SignatureAlgorithm;
 //import io.jsonwebtoken.security.Keys;
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,13 +15,14 @@ public class PaintShopApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
+//		Dotenv dotenv = Dotenv.load();
+//
+//		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+//		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//
+//		System.setProperty("jwtSecret", dotenv.get("jwtSecret"));
 
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-
-		System.setProperty("jwtSecret", dotenv.get("jwtSecret"));
 //		System.out.println("URL: " + System.getenv("DB_URL"));
 
 		SpringApplication.run(PaintShopApplication.class, args);
